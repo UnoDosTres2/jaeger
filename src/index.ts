99df0475258bcbf84de8858eb0b5f8d53370aec4
metehanalper <metehanalper03@gmail.com>
2024-02-12 01:17:46 +0300

    koa implementation denemeleri

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,29 @@
-import message from "./message";
+import Koa from "koa";
+import FactoryRepo from "./core/repos/FactoryRepo";
+import CreateFactory from "./core/useCases/CreateFactory";
+import Factory from "./core/entities/Factory";
+const app = new Koa();
 
-if (process.env.APP_START_BANNER) {
-  console.log(process.env.APP_START_BANNER);
-}
+// Middleware
 
-console.log(message);
+app.use(async (ctx, next) => {
+  ctx.status = 200;
+  console.log("Setting status");
+  // Call the next middleware, wait for it to complete
+  await next();
+});
+// Middleware 2
+app.use((ctx) => {
+  const factoryInput = {
+    domain: "ema-tech.net",
+    address: "Some Address",
+    name: "Entity Name",
+    phone: "1234567890",
+  };
+
+  //console.log(newFactory);
+  ctx.body = "Hello from Koa";
+});
+app.listen(3000, () => {
+  console.log(`Uygulama ${3000} portunda çalışıyor`);
+});
