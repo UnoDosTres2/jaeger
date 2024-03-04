536c96663d6d623016b9acd907b76e72e5c5d504
Ozan Müyesseroğlu <ozanmuyes@gmail.com>
2024-03-04 21:59:38 +0300

    Follow-up minor fix

--- src/context/backingServices/mysql.ts
+++ src/context/backingServices/mysql.ts
@@ -25,10 +25,6 @@ export default async (config: Config): Promise<[TeardownFn, _type]> => {
 
   const pool = mysql.createPool(poolOpts);
 
-  // TODO find out if those 2 below are needed
-  // await pool.connect();
-  // await pool.ping();
-
   return [
     async () => {
       await pool.end();
